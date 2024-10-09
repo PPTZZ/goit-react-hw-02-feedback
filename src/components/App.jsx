c99dda5c9fdb138c8d630c396222c2928621d7cd
Alex Poputa <poputa.alex@gmail.com>
2024-10-09 21:44:19 +0300

    added components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,33 @@
-import { Component } from "react";
-import Button from "./Button";
-import Notification from "./Notification";
+import { Component } from 'react';
+import Statistics from './Statistics';
+import Button from './Button';
+import Notification from './Notification';
 
 class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
-    total:0,
-  }
-  
-  incrementValue = ()=>{
+    total: 0,
+  };
 
-  }
+  incrementValue = () => {};
 
-  
-  render(){
-    // const { good,neutral,bad,total} = this.state
-    return(
+  render() {
+    const { total } = this.state;
+    
+    return (
       <>
-      <Notification message="There is no feedback"/>
-      <Button label="Positive" handleValue={this.incrementValue}/>
-      <Button label="Neutral" handleValue={this.incrementValue}/>
-      <Button label="Negative" handleValue={this.incrementValue}/>
+        {total > 0 ? (
+          <Statistics stats={this.state}/>
+        ) : (
+          <Notification message='There is no feedback' />
+        )}
+        <Button label='Positive' handleValue={this.incrementValue} />
+        <Button label='Neutral' handleValue={this.incrementValue} />
+        <Button label='Negative' handleValue={this.incrementValue} />
       </>
-    )
+    );
   }
 }
 
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+const Statistics = ({stats}) => {
+    const {good,neutral,bad,total} = stats;
+    return(
+        <>
+            <h1>Statistics</h1>
+            <h2>Posistive: {good}</h2>
+            <h2>Neutral:{neutral}</h2>
+            <h2>Negative:{bad}</h2>
+            <h2>Total:{total}</h2>
+            <h2>Posistive feedback:</h2>
+        </>
+    );
+};
+Statistics.propTypes = {
+    stats: PropTypes.object.isRequired
+  };
+export default Statistics;
